b48bb054523ff0f6f04dc47179071c610ef6b10f
xhf <763820835@qq.com>
2025-03-13 20:33:06 +0800

    用户注册登录

--- src/main/java/com/xiao/domain/DocumentChunks.java
+++ src/main/java/com/xiao/domain/DocumentChunks.java
@@ -2,61 +2,54 @@ package com.xiao.domain;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import java.sql.Blob;
 import java.util.Date;
 import lombok.Data;
 
 /**
  * 文档块表
  */
-@ApiModel(description = "文档块表")
+@ApiModel(description="文档块表")
 @Data
 public class DocumentChunks {
     /**
-     * 文档块ID
-     */
-    @ApiModelProperty(value = "文档块ID")
+    * 文档块ID
+    */
+    @ApiModelProperty(value="文档块ID")
     private Long id;
 
     /**
-     * 文档ID
-     */
-    @ApiModelProperty(value = "文档ID")
+    * 文档ID
+    */
+    @ApiModelProperty(value="文档ID")
     private Long documentId;
 
     /**
-     * 块索引
-     */
-    @ApiModelProperty(value = "块索引")
+    * 块索引
+    */
+    @ApiModelProperty(value="块索引")
     private Integer chunkIndex;
 
     /**
-     * 块内容
-     */
-    @ApiModelProperty(value = "块内容")
+    * 块内容
+    */
+    @ApiModelProperty(value="块内容")
     private String content;
 
     /**
-     * 元数据(JSON格式)
-     */
-    @ApiModelProperty(value = "元数据(JSON格式)")
+    * 元数据(JSON格式)
+    */
+    @ApiModelProperty(value="元数据(JSON格式)")
     private Object metadata;
 
     /**
-     * 向量嵌入(二进制格式)
-     */
-    @ApiModelProperty(value = "向量嵌入(二进制格式)")
-    private Blob vectorEmbedding;
-
-    /**
-     * 嵌入使用的模型
-     */
-    @ApiModelProperty(value = "嵌入使用的模型")
+    * 嵌入使用的模型
+    */
+    @ApiModelProperty(value="嵌入使用的模型")
     private String embeddingModel;
 
     /**
-     * 创建时间
-     */
-    @ApiModelProperty(value = "创建时间")
+    * 创建时间
+    */
+    @ApiModelProperty(value="创建时间")
     private Date createdAt;
 }
--- src/main/java/com/xiao/mapper/UserMapper.java
+++ src/main/java/com/xiao/mapper/UserMapper.java
@@ -1,18 +1,13 @@
 package com.xiao.mapper;
-import org.apache.ibatis.annotations.Param;
-import java.util.List;
 
 import com.xiao.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
-    User selectOneByUsername(@Param("username")String username);
-
-    List<User> selectByUsername(@Param("username")String username);
-
-    List<User> selectByUsernameAndPassword(@Param("username")String username,@Param("password")String password);
-
     int deleteByPrimaryKey(Long id);
 
     int insert(User record);
@@ -24,4 +19,10 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    User selectOneByUsername(@Param("username") String username);
+
+    List<User> selectByUsername(@Param("username") String username);
+
+    List<User> selectByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
